115c7761d5e832502b24caf3ee90c3922c581888
danielvinzia <danielvinzia@gmail.com>
2022-03-03 13:51:40 -0300

    clase de introduccion a PHP concluida

---
+++ 01-introduccion/001-introduccion_a_php/index.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+*comentario
+*de
+*varias
+*lineas
+*/
+class Answer
+{
+    /**
+     *comentario
+     *de
+     *varias
+     *lineas
+     */
+    protected $clients = [];  // MI COMENTARIO
+    /**
+     *comentario
+     *de
+     *varias
+     *lineas
+     */
+    protected $insurers = []; # OTRO COMENTARIO
+}
+
+?>
