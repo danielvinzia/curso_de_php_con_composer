eb5e744e092e34dcb14446827d06b5fd8febec3b
danielvinzia <danielvinzia@gmail.com>
2022-03-03 14:23:31 -0300

    clase de bloques de codigos sencillos en PHP concluida

---
+++ 01-introduccion/004-bloques_de_codigos_sencillos_en_php/bloques.php
@@ -0,0 +1,50 @@
+<?php
+
+$ejemplo = 4;
+
+if ($ejemplo > 4) {
+    # code...
+} elseif ($ejemplo < 4) {
+    # code...
+} else {
+    # code...
+}
+
+
+switch ($login) {
+    case true:
+        # code...
+        break;
+        
+    case false:
+        # code...
+        break;
+        
+    default:
+        # code...
+        break;
+}
+
+
+// consulta, usuarios
+foreach ($datos as $row) {
+    # code...
+}
+
+
+$a = 1;
+while ($a <= 10) {
+    # code...
+    
+    $a++;
+}
+
+
+do {
+    # code...
+} while ($a <= 10);
+
+
+for ($i = 0; $i < 10 ; $i++) { 
+    # code...
+}
