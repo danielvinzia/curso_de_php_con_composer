3ad73bee450b895aa42fc8147633a9bf87ca3d2f
danielvinzia <danielvinzia@gmail.com>
2022-03-03 14:10:57 -0300

    clase de sintaxis basica comparacion y variables en PHP concluida

---
+++ 01-introduccion/003-sintaxis_basica_comparacion_y_variables/index.php
@@ -0,0 +1,30 @@
+<?php
+
+// asignacion
+$num = 9;
+$lang = [
+    'es' => 'español',
+    'en' => 'ingles'
+];
+
+// aritmetica
+echo "Suma 2 + 2 " . ((int) 2 + (int) 2);
+echo "Resta 2 - 2 " . ((int) 2 - (int) 2);
+echo "Multiplica 2 * 2 " . 2 * 2;
+echo "Divide 2 / 2 " . 2 / 2;
+echo "Modulo 2 % 2 " . 2 % 2;
+echo "Exponencial 2 ** 2 " . 2 ** 2;
+
+// comparacion
+// Igual ==, valor '9' == 9
+// Igual ===, valor 9 === 9
+// Diferencia !=, valor
+// Diferencia !==, valor - tipo
+// <, >, <=, >=
+
+// Variables variable
+$app = 'name';
+$name = 'kolo';
+
+echo $app; // name
+echo $$app; // kolo
