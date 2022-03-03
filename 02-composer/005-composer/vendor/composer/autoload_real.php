606b4ba79b131c785e0a582b4511c85af1c70132
danielvinzia <danielvinzia@gmail.com>
2022-03-03 15:26:15 -0300

    clase de gestion de paquetes en PHP concluida

--- 02-composer/005-composer/vendor/composer/autoload_real.php
+++ 02-composer/005-composer/vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit57fd638eea75c9f8a0d5decf69134f84
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit57fd638eea75c9f8a0d5decf69134f84', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit57fd638eea75c9f8a0d5decf69134f84', 'loadClassLoader'));
