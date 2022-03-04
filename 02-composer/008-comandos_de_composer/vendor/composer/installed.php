c2e0dd1070761ca705e6bcbc4b336dddb6b6636d
danielvinzia <danielvinzia@gmail.com>
2022-03-04 10:05:32 -0300

    clase de comandos de composer en PHP concluida

---
+++ 02-composer/008-comandos_de_composer/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'ec5568a07c5437167ae0b2eade0ec81a5213984e',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'ec5568a07c5437167ae0b2eade0ec81a5213984e',
+            'dev_requirement' => false,
+        ),
+    ),
+);
