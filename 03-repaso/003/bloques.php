f30c53151492d748876446d96d591fc4f7a43e89
danielvinzia <danielvinzia@gmail.com>
2022-03-21 11:36:20 -0300

    Bloques de codigo

---
+++ 03-repaso/003/bloques.php
@@ -0,0 +1,42 @@
+<?php
+$b = 5;
+if ($b > 10) {
+    # code...
+} elseif ($b < 10) {
+    # code...
+} else {
+    # code...
+}
+
+switch ($login) {
+    case true:
+        # code...
+        break;
+    case false:
+        # code...
+        break;
+    default:
+        # code...
+        break;
+}
+
+// consulta, usuarios
+foreach ($datos as $key => $name) {
+    # code...
+}
+
+$a = 1000;
+while ($a <= 10) {
+    # code...
+    
+    $a++;
+}
+
+
+do {
+    # code...
+} while ($a <= 10);
+
+for ($i = 0; $i < 10; $i++) { 
+    # code...
+}
