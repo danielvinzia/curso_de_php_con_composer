7ccc82c9c18afe7182028af22023212c05e099b0
danielvinzia <danielvinzia@gmail.com>
2022-03-21 10:58:54 -0300

    clase introduccion

---
+++ 03-repaso/001/index.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * comentario de varias
+ * lineas para 
+ * palabra
+ */
+class Answer
+{
+    /**
+     * comentario de varias
+     * lineas para 
+     * palabra
+     */
+    protected $clients = []; // Mi Comentario
+
+    /**
+     * comentario de varias
+     * lineas para 
+     * palabra
+     */
+    protected $insurers = []; # Mi otro comentario
+}
