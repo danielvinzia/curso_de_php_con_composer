4a54d5c3162187f21b3be0574f79b90435f91066
danielvinzia <danielvinzia@gmail.com>
2022-03-21 11:25:42 -0300

    sintaxis basica parte 2

--- 03-repaso/002/index.php
+++ 03-repaso/002/index.php
@@ -14,3 +14,16 @@ echo "Multiplica 2 * 2 " . 2 * 2;
 echo "Divide 2 / 2 " . 2 / 2;
 echo "Modulo 2 % 2 " . 2 % 2;
 echo "Exponencial 2 ** 2 " . 2 ** 2;
+
+// Comparacion
+// igual ==, valor '9' == 9
+// igual ===, valor - tipo 9 == 9
+// Diferencias !=, valor
+// Diferencias !==, valor - valor
+// <, >, <=, >=
+
+// Variables variables
+$app = 'name';
+$name = 'Daniel';
+echo $app; // name
+echo $$app; // Daniel
