99a2b0da07e024c532ad6e93525cfa9d3d114570
danielvinzia <danielvinzia@gmail.com>
2022-03-21 11:20:02 -0300

    sintaxis basica

---
+++ 03-repaso/002/index.php
@@ -0,0 +1,16 @@
+<?php
+
+// Asingnacion
+$num = 9;
+$lang = [
+    'es' => 'español',
+    'en' => 'ingles'
+];
+
+// Aritmetica
+echo "Suma 2 + 2 " . ((int) 2 + (int) 2);
+echo "Resta 2 - 2 " . ((int) 2 - (int) 2);
+echo "Multiplica 2 * 2 " . 2 * 2;
+echo "Divide 2 / 2 " . 2 / 2;
+echo "Modulo 2 % 2 " . 2 % 2;
+echo "Exponencial 2 ** 2 " . 2 ** 2;
