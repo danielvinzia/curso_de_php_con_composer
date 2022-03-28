17a51e41a616b639694fe07b196df4165de77820
danielvinzia <danielvinzia@gmail.com>
2022-03-28 10:46:24 -0300

    comandos de composer en PHP

---
+++ 03-repaso/007/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '09b01b85b3c5d7792ad620a2faced0724dc59f0c',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '09b01b85b3c5d7792ad620a2faced0724dc59f0c',
+            'dev_requirement' => false,
+        ),
+    ),
+);
